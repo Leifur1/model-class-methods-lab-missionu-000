ea80d9d0abe420fa6a27d67d86c9a75abdc8ebf9
Leifur Sigurdsson <leifurerik@gmail.com>
2019-02-26 22:49:47 +0000

    Automatically backed up by Learn

--- app/models/boat_classification.rb
+++ app/models/boat_classification.rb
@@ -1,4 +1,21 @@
 class BoatClassification < ActiveRecord::Base
   belongs_to :boat
   belongs_to :classification
+
+  def self.first_five
+    all.limit(5)
+  end
+
+  def self.dinghy
+    where(length < 20)
+  end
+
+  def self.ship
+    where(length > 20)
+  end
+
+  def self.last_three_alphabetically
+    all.order(name: :desc).limit(3)
+  end
+
 end
